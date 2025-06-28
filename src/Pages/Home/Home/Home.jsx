d4f84a7a3687020368b0734d9d5ff16fdf2f1ca6
rafiahmedrifatt <rafiahmed0471@gmail.com>
2025-06-29 02:23:11 +0600

    added few more sections

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HeroSection from '../HeroSection/HeroSection';
 import AboutSection from '../AboutSection/AboutSection';
 import SkillsSection from '../SkillsSection/SkillsSection';
+import ProjectSection from '../ProjectsSection/ProjectSection';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <HeroSection />
             <AboutSection />
             <SkillsSection />
+            <ProjectSection />
         </div>
     );
 };
